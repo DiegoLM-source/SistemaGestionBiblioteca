5581126356060b9c7f3c2786a70f09ec94b31c43
Diego <diegolionelml@gmail.com>
2026-04-23 13:50:16 -0500

    Solucion del problema de eliminar prestamos y de prestar stock negativo o 0

--- frontend/src/services/prestamoServices.js
+++ frontend/src/services/prestamoServices.js
@@ -6,4 +6,4 @@ export const getPrestamos = () => axios.get(BASE);
 export const getPrestamoById = (id) => axios.get(`${BASE}/${id}`);
 export const createPrestamo = (data) => axios.post(BASE, data);
 export const cambiarEstado = (id, estado)=> axios.patch(`${BASE}/${id}/estado`, { estado });
-export const deletePrestamo = (id) => axios.delete(`${BASE}/${id}/estado`, {estado});
+export const deletePrestamo = (id) => axios.delete(`${BASE}/${id}`);
