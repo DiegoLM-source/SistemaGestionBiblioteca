8062dd351a251150c24df2bc06ee1b5dd06a4fcd
Diego <diegolionelml@gmail.com>
2026-05-09 02:42:42 -0500

    CORS en server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = require('./app');
 
 const authRoutes = require('./routes/authRoutes');
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
