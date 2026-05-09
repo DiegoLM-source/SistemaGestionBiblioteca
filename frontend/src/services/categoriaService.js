7597b58ca224166c97666d3f1eb23d533af3f29d
Diego <diegolionelml@gmail.com>
2026-05-09 03:30:07 -0500

    Espero que sea el ultimo

--- frontend/src/services/categoriaService.js
+++ frontend/src/services/categoriaService.js
@@ -1,4 +1,7 @@
 import axios from "axios";
+import { API_URL } from "../config";
+
 const BASE = `${API_URL}/categorias`;
+
 export const getCategorias = () => axios.get(BASE);
 export const createCategoria = (data) => axios.post(BASE, data);
--- frontend/src/services/estanteService.js
+++ frontend/src/services/estanteService.js
@@ -1,4 +1,7 @@
 import axios from "axios";
+import { API_URL } from "../config";
+
 const BASE = `${API_URL}/estantes`;
+
 export const getEstantes = () => axios.get(BASE);
 export const createEstante = (data) => axios.post(BASE, data);
